5583572d80fd5397cc7ad709ffef30d5254a445b
Dmitrii Morozov <33morozov@gmail.com>
2023-01-01 20:31:15 +0300

    Add mptt-model for menus and simple view for model

---
+++ menu_builder/admin.py
@@ -0,0 +1,15 @@
+from mptt.admin import DraggableMPTTAdmin
+from menu_builder.models import menu
+
+
+admin.site.register(
+    menu,
+    DraggableMPTTAdmin,
+    list_display=(
+        'tree_actions',
+        'indented_title',
+    ),
+    list_display_links=(
+        'indented_title',
+    ),
+)
--- menu_builder/view.py
+++ menu_builder/view.py
@@ -1,5 +1,7 @@
 from django.views.generic.base import TemplateView
+from menu_builder.models import menu
 
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+    context = {'menus': menu.objects.all()}
