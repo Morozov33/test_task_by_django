191a77b6eae0e3ca79e74c8e8f9af55aa6b29a86
Dmitrii Morozov <33morozov@gmail.com>
2023-01-02 20:19:31 +0300

    Add migrations for Menu model

--- menu_builder/models.py
+++ menu_builder/models.py
@@ -1,13 +1,24 @@
 from django.db import models
+from django.urls import reverse, NoReverseMatch
 from mptt.models import MPTTModel, TreeForeignKey
 
 
 class menu(MPTTModel):
     name = models.CharField(max_length=50, unique=True)
+    url = models.CharField(max_length=255, verbose_name="URL", null=True)
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
 
     def __str__(self):
-        return self.name
+        return f"{self.name}"
 
     class MPTTMeta:
-        order_insertion_by = ['name']
+        order_insertion_by = ['name', 'url']
+
+    def get_absolute_url(self):
+        if self.url:
+            try:
+                return reverse(self.url)
+            except NoReverseMatch:
+                return self.url
+        else:
+            return reverse("home")
