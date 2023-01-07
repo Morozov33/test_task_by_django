7de00019ae933845e0518a0ceaaf47cc88021854
Dmitrii Morozov <33morozov@gmail.com>
2023-01-07 22:19:00 +0300

    Delete staticfiles from settings

--- menu_builder/settings.py
+++ menu_builder/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'django_bootstrap5',
     'mptt',
 ]
@@ -129,11 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-fileso/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
