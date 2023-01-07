d309806c994e157499d56ffba5cf5db708d78ceb
Dmitrii Morozov <33morozov@gmail.com>
2023-01-07 22:35:28 +0300

    Add Heroku in allowded hosts in settings

--- menu_builder/settings.py
+++ menu_builder/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['webserver', '127.0.0.1', 'menu-builder.herokuapp.com', ]
+ALLOWED_HOSTS = ['webserver', '127.0.0.1', 'menu-builder-django.herokuapp.com', ]
 
 
 # Application definition
