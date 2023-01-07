45f5ec2dd5908a913236c1bfd3145545d88cbf80
Dmitrii Morozov <33morozov@gmail.com>
2023-01-07 17:16:00 +0300

    Fixed settings for deploy

--- menu_builder/settings.py
+++ menu_builder/settings.py
@@ -127,9 +127,13 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
+# https://docs.djangoproject.com/en/4.1/howto/static-fileso/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
